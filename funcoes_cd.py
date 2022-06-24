d956f3bc94987c863ecfd66075a7edb2422d9baf
marcus-vn-santos <106926057+marcus-vn-santos@users.noreply.github.com>
2022-06-23 22:27:02 -0300

    Update funcoes_cd.py

--- funcoes_cd.py
+++ funcoes_cd.py
@@ -1,16 +1,16 @@
 def mesmoCartao (cartaoDesejado,cartaoAtual):
     if cartaoDesejado == cartaoAtual:
-        return print((f'O seu cartão atual já é da categoria {cartaoAtual}'))
+        return (f'O seu cartão atual já é da categoria {cartaoAtual}')
 
 def novoCartao (cartaoDesejado,renda,score):
     if cartaoDesejado == 'Platinum' and renda >= 5000 and score >= 750:
-        return print(('O seu perfil é eligível para esse cartão. Em até 10 minutos o cartão estará disponível no app para solicitar.'))
+        return ('O seu perfil é eligível para esse cartão. Em até 10 minutos o cartão estará disponível no app para solicitar.')
     elif cartaoDesejado == 'Black' and renda >= 10000 and score >= 850:
-        return print(('O seu perfil é eligível para esse cartão. Em até 10 minutos o cartão estará disponível no app para solicitar.'))
+        return ('O seu perfil é eligível para esse cartão. Em até 10 minutos o cartão estará disponível no app para solicitar.')
     elif cartaoDesejado == 'VIP' and renda >= 20000 and score >= 950:
-        return print(('O seu perfil é eligível para esse cartão. Em até 10 minutos o cartão estará disponível no app para solicitar.'))
+        return ('O seu perfil é eligível para esse cartão. Em até 10 minutos o cartão estará disponível no app para solicitar.')
     else: 
-        return print(('Infelizmente, você não se enquarda no perfil necessário.'))
+        return ('Infelizmente, você não se enquarda no perfil necessário.')
 
 
 def InformativoBeneficiosGold(cartaoAtual):
@@ -61,25 +61,25 @@ def InformativoBeneficiosVIP(cartaoAtual):
 
 def venc (dataFechamento):
     if dataFechamento == 5:
-        return print(('A data de fechamento da sua fatura é todo dia 15 de cada mês.'))
+        return ('A data de fechamento da sua fatura é todo dia 15 de cada mês.')
     elif dataFechamento == 8:
-        return print(('A data de fechamento da sua fatura é todo dia 18 de cada mês.'))
+        return ('A data de fechamento da sua fatura é todo dia 18 de cada mês.')
     elif dataFechamento == 10:
-        return print(('A data de fechamento da sua fatura é todo dia 18 de cada mês.'))
+        return ('A data de fechamento da sua fatura é todo dia 18 de cada mês.')
     elif dataFechamento == 20:
-        return print(('A data de fechamento da sua fatura é todo dia 27 de cada mês.'))
+        return ('A data de fechamento da sua fatura é todo dia 27 de cada mês.')
     elif dataFechamento == 25:
-        return print(('A data de fechamento da sua fatura é todo dia 30 de cada mês.'))                    
+        return ('A data de fechamento da sua fatura é todo dia 30 de cada mês.')                    
     
 
 def melhorCompra (dataFechamento):
     if dataFechamento == 5:
-        return print(('A melhor data de compra é todo dia 6 de cada mês.'))
+        return ('A melhor data de compra é todo dia 6 de cada mês.')
     elif dataFechamento == 8:
-        return print(('A melhor data de compra é todo dia 9 de cada mês.'))
+        return ('A melhor data de compra é todo dia 9 de cada mês.')
     elif dataFechamento == 10:
-        return print(('A melhor data de compra é todo dia 11 de cada mês.'))
+        return ('A melhor data de compra é todo dia 11 de cada mês.')
     elif dataFechamento == 20:
-        return print(('A melhor data de compra é todo dia 21 de cada mês.'))
+        return ('A melhor data de compra é todo dia 21 de cada mês.')
     elif dataFechamento == 25:
-        return print(('A melhor data de compra é todo dia 26 de cada mês.'))
+        return ('A melhor data de compra é todo dia 26 de cada mês.')
